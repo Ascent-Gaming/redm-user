9a8506ec2625a06b94b4014fa2dd023b367410f6
Ryan Lockard <ryan.jesse.lockard@gmail.com>
2020-10-27 00:36:21 -0400

    resource functionality v0.0.1

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,25 +1,45 @@
 import { Delay } from "../util"
+import { UserClientClass } from "./models"
 
-on("onClientResourceStart", () => {
-  // Show the entire RDR3 Map
-  Citizen.invokeNative("0x4B8F743A4A6D2FF8", true)
+// -- [ Initialize the base User class (client-side) ] --
+let User: UserClientClass = undefined
+on("onClientResourceStart", (resourceName: string) => {
+  if (resourceName == GetCurrentResourceName()) {
+    User = new UserClientClass()
+    User.NetworkActive = false
+  }
 })
 
 // -- [ Loop checking for when a player becomes "ACTIVE" on the network. ] --
-let NetworkActive = false
-const NetworkTick = setTick(async () => {
-  while (!NetworkActive) {
+const networkActiveTick = setTick(async () => {
+  while (!User.NetworkActive) {
     const
       playerPedId = PlayerPedId(),
       playerId = NetworkGetPlayerIndexFromPed(playerPedId),
       isNetworkActive = NetworkIsPlayerActive(playerId)
 
     if (isNetworkActive) {
-      emitNet("user:networkActive")
-      global.exports['spawnmanager']['spawnPlayer']()
-      NetworkActive = true
-      clearTick(NetworkTick)
+      // Emit events to signal a user is activley connected to the network.
+      const userActiveEvent: UserActivatedEvent = {
+        playerPedId: playerPedId,
+        playerId: playerId
+      }
+      emitNet("user:networkActive", userActiveEvent)
+      emit("user:networkActive", userActiveEvent)
+
+      // Update the global User object
+      User.NetworkActive = true
+
+      // Cleanup
+      clearTick(networkActiveTick)
     }
     await Delay(1000)
   }
+})
+
+on("user:networkActive", (event: UserActivatedEvent) => {
+  DoScreenFadeIn(500)
+  ShutdownLoadingScreen()
+  SetEntityCoords(event.playerPedId, 1.505, 1015.63, 208.4088, false, false, false, true)
+  FreezeEntityPosition(event.playerPedId, false)
 })
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,35 +1,55 @@
 import * as Config from "../../config.json"
+import { UserServerClass } from "./models"
 
-on('playerConnecting', (name, setKickReason, deferrals) => {
+// -- [ Initialize the base User Map ] --
+let UserMap: Map<string, UserServerClass> = undefined
+on("onResourceStart", (resourceName: string) => {
+  if (resourceName == GetCurrentResourceName()) {
+    UserMap = new Map<string, UserServerClass>()
+  }
+})
+
+on("onResourceStop", (resourceName: string) => {
+  if (resourceName == GetCurrentResourceName()) {
+    UserMap.clear()
+  }
+})
+
+// -- [ Defer the Player's connection; ensuring they are connecting with Steam open.
+//      Will also populate the UserMap described above with the players's "name", "ip", and "steamId" ] --
+on("playerConnecting", (name, setKickReason, deferrals) => {
+  emit("utilities:logServer", {
+    level: "info",
+    title: "User-Authorization",
+    output: `Authorizing User: "${name}" using SteamID.`,
+  })
+
   deferrals.defer()
 
-  const player = global.source
+  const player: string = global.source
 
   setTimeout(() => {
-    deferrals.update(`Authenticating SteamID for ${Config.server_name}.`)
-  
-    let steamIdentifier = null
+    deferrals.update(`Authenticating SteamID for ${Config.user.serverName}`)
+
+    // Collect the player's SteamID for authorization
+    let
+      steamIdentifier = null
 
     for (let i = 0; i < GetNumPlayerIdentifiers(player); i++) {
       const identifier = GetPlayerIdentifier(player, i)
-  
-      if (identifier.includes('steam:')) {
+
+      if (identifier.includes("steam:")) {
         steamIdentifier = identifier
       }
     }
 
+    // Defer the connection, either disallowing it or allowing it to progress.
     setTimeout(() => {
       if (steamIdentifier === null) {
-        deferrals.done('Unable to authenticate SteamID. Relaunch RedM and make sure you have the Steam Client open.')
+        deferrals.done("Unable to authenticate SteamID. Relaunch RedM and make sure you have the Steam Client open.")
         return
       } else {
         deferrals.done()
-  
-        // global.exports['mysql-async']['mysql_insert'](
-        //   'INSERT INTO user (steamId) VALUES (@steamId)',
-        //   {'@steamId': steamIdentifier},
-        //   (res) => {console.log(res)}
-        // )
       }
     }, 0)
 
@@ -37,7 +57,79 @@ on('playerConnecting', (name, setKickReason, deferrals) => {
 
 })
 
-onNet("user:playerSpawned", () => {
+// -- [ Destroy the reference to this player in the UserMap ] --
+on("playerDropped", (reason: string) => {
   const player = global.source
-  console.log("gotcha!")
+
+  emit("utilities:logServer", {
+    level: "debug",
+    title: "User",
+    output: `User ${GetPlayerName(player)} has disconnected. Reason: ${reason}`
+  })
+
+  UserMap.delete(player)
 })
+
+// -- [ React to the player being activley on the Network; ensures their information is populated in the MySQL DB. ] --
+onNet("user:networkActive", (event: UserActivatedEvent) => {
+  const player = global.source
+
+  // Collect the player's SteamID and IP for future reference
+  let
+    steamIdentifier = null,
+    ipAddress = null
+
+  for (let i = 0; i < GetNumPlayerIdentifiers(player); i++) {
+    const identifier = GetPlayerIdentifier(player, i)
+
+    if (identifier.includes("steam:")) {
+      steamIdentifier = identifier.substring(6, identifier.length)
+    }
+
+    if (identifier.includes("ip:")) {
+      ipAddress = identifier.substring(3, identifier.length)
+    }
+  }
+
+  UserMap.set(player, {
+    steamId: steamIdentifier,
+    ip: ipAddress
+  })
+
+  // Check if the player's account is already registered in the DB.
+  emit("utilities:logServer", {
+    level: "debug",
+    title: "User-Authentication",
+    output: "Authenticating user's existence in Database records.",
+  })
+
+  global.exports['mysql-async']['mysql_fetch_all'](
+      "SELECT * FROM user WHERE steamId = @steamId",
+      {"@steamId": steamIdentifier},
+      (res: []) => onFetchUserExist(player, res)
+  )
+})
+
+const onFetchUserExist = (playerId: string, response: []) => {
+  // User does not exist, create them
+  if (response.length == 0) {
+    const user = UserMap.get(playerId)
+
+    global.exports['mysql-async']['mysql_execute'](
+      "INSERT INTO user (steamId, ip) VALUES (@steamId, @ip)",
+      {
+        "@steamId": user.steamId,
+        "@ip": user.ip
+      },
+      (res: number) => onInsertUser(playerId, res)
+    )
+  }
+}
+
+const onInsertUser = (playerId: string, response: number) => {
+  emit("utilities:logServer", {
+    level: "info",
+    title: "User-Authentication",
+    output: `New USER created in DB. SteamID: ${UserMap.get(playerId).steamId}`
+  })
+}
