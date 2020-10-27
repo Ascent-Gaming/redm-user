301051007d11668f7b11a570b4f7f9f56cd907a3
Ryan Lockard <ryan.jesse.lockard@gmail.com>
2020-10-27 00:35:41 -0400

    added UserActivedEvent

---
+++ src/@types/declarations.d.ts
@@ -0,0 +1,11 @@
+/**
+ * Define common `Types` here to be automatically avialable in both the
+ * `/client/` and `/server/` directories.
+ * 
+ * A common example would be `Events` passed between the client and server.
+ */
+
+type UserActivatedEvent = {
+  playerPedId: number
+  playerId: number
+}
